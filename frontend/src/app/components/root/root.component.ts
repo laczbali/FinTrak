52c09624ab18394db0582b2fcdfa43ee1688d567
Balazs Laczko <laczbali@gmail.com>
2022-10-19 17:14:26 +0200

    added router animations

--- frontend/src/app/components/root/root.component.ts
+++ frontend/src/app/components/root/root.component.ts
@@ -1,15 +1,59 @@
+import { animate, group, query, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './root.component.html',
-  styleUrls: ['./root.component.scss']
+  styleUrls: ['./root.component.scss'],
+  "animations": [
+    trigger('routeAnimation', [
+      transition('* <=> *', [
+        style({ position: 'relative' }),
+
+        query(':enter, :leave', [
+          style({
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%'
+          })
+        ], {optional: true}),
+
+        query(':enter', [
+          style({ left: '100%' })
+        ], { optional: true }),
+
+        group([
+          query(':leave', [
+            animate('200ms ease-out', style({ left: '-100%', opacity: 0 }))
+          ], { optional: true }),
+          
+          query(':enter', [
+            animate('300ms ease-out', style({ left: '0%' }))
+          ], { optional: true })
+        ])
+      ])
+    ])
+  ]
 })
 export class RootComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {
+    this.router.events.subscribe((val) => {
+      if (val instanceof NavigationEnd) {
+        this.currentRoute = val.url;
+      }
+    })
+  }
+
+  public currentRoute: string = '';
 
   ngOnInit() {
+  }
+
+  getRouteAnimationState(): string {
+    return '';
   }
 
 }
